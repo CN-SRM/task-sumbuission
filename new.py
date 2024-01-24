c7b0b27fcc45449eb76b886f2a08050b699681cf
nabhpatodi10 <nabhpatodi1005@gmail.com>
2024-01-25 02:08:48 +0530

    Adding Files and Model

--- new.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    # Base case: factorial of 0 or 1 is 1
-    if n == 0 or n == 1:
-        return 1
-    
-    # Recursive case: multiply n with factorial of (n-1)
-    return n * factorial(n - 1)
